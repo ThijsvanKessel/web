09b3ce465e9d9ce2a16974724f5b79b8324b3c84
Thijs <thijsvankessel@hotmail.nl>
2020-03-09 17:37:35 +0100

    Made Beginning

--- 2019 - 2020/Portfolio 2020/index.php
+++ 2019 - 2020/Portfolio 2020/index.php
@@ -4,8 +4,10 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <!-- custom CSS -->
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>Portfolio</title>
@@ -16,6 +18,16 @@
             include("include/header.php");
             headerFunction();
         ?>
-        <img src="images/kirby.jpg">
+        <div id="AboutMe">
+            <h3>About Me:</h3>
+            <div id="TextContainer" class="col-8">
+                <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</a>
+            </div>
+            <img src="images/kirby.jpg" id="Photo" class="col-3">
+        </div>
+        <div id="FirstBackGround">
+        </div>
+        <div id="SecondBackGround">
+        </div>
     </body>
 </html>
